9b7474c8918b893af41ce9e0fbe76be0e6caad55
Morten Frederiksen <mrtn.frederiksen@gmail.com>
2021-02-05 14:18:35 +0100

    initial commit - default projects

---
+++ service-b/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace service_b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
