1733317d5d7a96350dee4772dbb9158f358299cf
Morten Frederiksen <mrtn.frederiksen@gmail.com>
2021-02-06 10:04:50 +0100

    improve comments

--- ApiGateway/Startup.cs
+++ ApiGateway/Startup.cs
@@ -27,19 +27,23 @@ namespace ApiGateway
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Use web api controllers
             services.AddControllers();
+
+            // Use swagger
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api Gateway", Description = "Microservice architecture playground project using .NET 5, RabbitMQ, Docker.", Version = "v1" });
             });
 
-            // Dependency injection
+            // Setup dependency injection
             services.AddSingleton<IMQ>(mq => new MQ());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // During development show exceptions
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
